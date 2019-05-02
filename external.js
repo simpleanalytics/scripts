0692757b6abe05870d12b65030b9a14e2a0a1656
Adriaan van Rossum <git@adriaan.io>
2019-05-02 20:19:35 +0800

    Remove semicolons and add comments

--- external.js
+++ external.js
@@ -1,17 +1,18 @@
+/* eslint-env browser */
+
 (function(window, hostname) {
-  if (!window) return;
-  var nav = window.navigator;
-  var loc = window.location;
-  var host = loc.hostname;
-  var doc = window.document;
-  var con = window.console;
+  if (!window) return
+  var nav = window.navigator
+  var loc = window.location
+  var host = loc.hostname
+  var doc = window.document
+  var con = window.console
 
   try {
-    var userAgent = nav.userAgent;
-    var dis = window.dispatchEvent;
-    var lastSendUrl;
+    var userAgent = nav.userAgent
+    var dis = window.dispatchEvent
+    var lastSendUrl
     var baseUrl = 'http://localhost/external'
-    // var baseUrl = 'https://' + hostname + path
     var notSending = 'Not sending requests '
 
     var attr = function(script, attribute) { return script && script.getAttribute('data-' + attribute) }
@@ -23,45 +24,45 @@
 
     // A simple log function so the user knows why a request is not being send
     var warn = function(message) {
-      if (con && con.warn) con.warn(' :scitylanA elpmiS'.split('').reverse().join('') + message);
+      if (con && con.warn) con.warn(' :scitylanA elpmiS'.split('').reverse().join('') + message)
     }
 
     // // Don't track when host is localhost
-    if (host === 'localhost') return warn(notSending + 'from localhost');
+    if (host === 'localhost') return warn(notSending + 'from localhost')
 
     // We do advanced bot detection in our API, but this line filters already most bots
-    if (userAgent.search(/(bot|spider|crawl)/ig) > -1) return warn(notSending + 'because user agent is a robot');
+    if (userAgent.search(/(bot|spider|crawl)/ig) > -1) return warn(notSending + 'because user agent is a robot')
 
     var getRef = function() {
       // From the search we grab the utm_source and ref and save only that
-      var refMatches = loc.search.match(/[?&](utm_source|source|ref)=([^?&]+)/gi);
-      var refs = refMatches ? refMatches.map(function(m) { return m.split('=')[1] }) : [];
+      var refMatches = loc.search.match(/[?&](utm_source|source|ref)=([^?&]+)/gi)
+      var refs = refMatches ? refMatches.map(function(m) { return m.split('=')[1] }) : []
       if (refs && refs[0]) return refs[0]
     }
 
     var post = function(isPushState) {
       // Obfuscate personal data in URL by dropping the search and hash
-      var url = loc.protocol + '//' + host + loc.pathname;
+      var url = loc.protocol + '//' + host + loc.pathname
 
       // Add hash to url when script is put in to hash mode
-      if (mode === 'hash' && loc.hash) url += loc.hash.split('?')[0];
+      if (mode === 'hash' && loc.hash) url += loc.hash.split('?')[0]
 
       // Don't send the last URL again (this could happen when pushState is used to change the URL hash or search)
-      if (lastSendUrl === url) return;
-      lastSendUrl = url;
+      if (lastSendUrl === url) return
+      lastSendUrl = url
 
       // Skip prerender requests
-      if ('visibilityState' in doc && doc.visibilityState === 'prerender') return warn(notSending + 'when prerender');
+      if ('visibilityState' in doc && doc.visibilityState === 'prerender') return warn(notSending + 'when prerender')
 
       // Don't track when Do Not Track is set to true
-      if (!skipDNT && 'doNotTrack' in nav && nav.doNotTrack === '1') return warn(notSending + 'when doNotTrack is enabled');
+      if (!skipDNT && 'doNotTrack' in nav && nav.doNotTrack === '1') return warn(notSending + 'when doNotTrack is enabled')
 
       var ref = getRef()
-      var data = { url: url };
-      if (userAgent) data.ua = userAgent;
-      if (ref) data.urlReferrer = ref;
-      if (doc.referrer && !isPushState) data.referrer = doc.referrer;
-      if (window.innerWidth) data.width = window.innerWidth;
+      var data = { url: url }
+      if (userAgent) data.ua = userAgent
+      if (ref) data.urlReferrer = ref
+      if (doc.referrer && !isPushState) data.referrer = doc.referrer
+      if (window.innerWidth) data.width = window.innerWidth
 
       try {
         data.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
@@ -69,33 +70,33 @@
         // nothing
       }
 
-      var request = new XMLHttpRequest();
-      request.open('POST', hostname, true);
+      var request = new XMLHttpRequest()
+      request.open('POST', hostname, true)
 
       // We use content type text/plain here because we don't want to send an
       // pre-flight OPTIONS request
-      request.setRequestHeader('Content-Type', 'text/plain; charset=UTF-8');
-      request.send(JSON.stringify(data));
+      request.setRequestHeader('Content-Type', 'text/plain; charset=UTF-8')
+      request.send(JSON.stringify(data))
     }
 
     // Thanks to https://gist.github.com/rudiedirkx/fd568b08d7bffd6bd372
-    var his = window.history;
-    var hisPushState = his ? his.pushState : null;
+    var his = window.history
+    var hisPushState = his ? his.pushState : null
     if (hisPushState && Event && dis) {
       var stateListener = function(type) {
-        var orig = his[type];
+        var orig = his[type]
         return function() {
-          var rv = orig.apply(this, arguments);
-          var event = new Event(type);
-          event.arguments = arguments;
-          dis(event);
-          return rv;
-        };
-      };
-      his.pushState = stateListener('pushState');
+          var rv = orig.apply(this, arguments)
+          var event = new Event(type)
+          event.arguments = arguments
+          dis(event)
+          return rv
+        }
+      }
+      his.pushState = stateListener('pushState')
       window.addEventListener('pushState', function() {
-        post(true);
-      });
+        post(true)
+      })
     }
 
     // When in hash mode, we record a pageview based on the onhashchange function
@@ -110,7 +111,7 @@
       queue.push(a)
     }
 
-    // Create iframe to only put the data on their subdomain, not on the main domain
+    // Only load the iframe when events are pushed
     var loading = false
     var loadIframe = function() {
       loading = true
@@ -129,11 +130,12 @@
       doc.body.appendChild(iframe)
     }
 
-    post();
+    // Post the page view
+    post()
   } catch (e) {
-    if (con && con.error) con.error(e);
-    var url = hostname + '/image.gif';
-    if (e && e.message) url = url + '?error=' + encodeURIComponent(e.message);
-    new Image().src = url;
+    if (con && con.error) con.error(e)
+    var url = hostname + '/image.gif'
+    if (e && e.message) url = url + '?error=' + encodeURIComponent(e.message)
+    new Image().src = url
   }
-})(window, 'http://simpleanalytics.example.com:8000');
+})(window, 'http://simpleanalytics.example.com:8000')
