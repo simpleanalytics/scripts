100400533af7ce8f6fcdec218726ea53bfc602d2
Adriaan van Rossum <git@adriaan.io>
2019-12-04 18:35:03 +0100

    Calculate duration better with document.hidden and add onpopstate for navigation buttons and some cleanup

--- src/default.js
+++ src/default.js
@@ -1,3 +1,5 @@
+/* eslint-env browser */
+
 (function(window, version, script, endpoint) {
   if (!window) return;
 
@@ -8,16 +10,14 @@
   var doNotTrack = "doNotTrack";
   var pageviews = "pageviews";
   var events = "events";
+  var slash = "/";
   var nav = window.navigator;
+  var userAgent = nav.userAgent;
   var loc = window.location;
   var doc = window.document;
   var hostname = loc.hostname;
-  var payload = {
-    version: version,
-    hostname: hostname,
-    pageviews: [],
-    events: []
-  };
+  var notSending = "Not sending requests ";
+  var thousand = 1000;
 
   /** if spa **/
   var pushState = "pushState";
@@ -38,34 +38,102 @@
     if (con && con.warn) con.warn("Simple Analytics: " + message);
   };
 
-  var getParams = function(regex) {
-    // From the search we grab the utm_source and ref and save only that
-    var matches = loc.search.match(
-      new RegExp("[?&](" + regex + ")=([^?&]+)", "gi")
-    );
-    var match = matches
-      ? matches.map(function(m) {
-          return m.split("=")[1];
-        })
-      : [];
-    if (match && match[0]) return match[0];
+  var scriptElement = doc.querySelector(
+    'script[src="' + protocol + script + '"]'
+  );
+  var attr = function(scriptElement, attribute) {
+    return scriptElement && scriptElement.getAttribute("data-" + attribute);
   };
 
-  var secondsSince = function(since) {
-    return Math.round((Date.now() - since) / 1000);
-  };
+  var mode = attr(scriptElement, "mode");
+  var skipDNT = attr(scriptElement, "skip-dnt") === "true";
+  var functionName = attr(scriptElement, "sa-global") || "sa";
+
+  // Don't track when Do Not Track is set to true
+  if (!skipDNT && doNotTrack in nav && nav[doNotTrack] === "1")
+    return warn(notSending + "when " + doNotTrack + " is enabled");
+
+  // Don't track when localhost
+  if (hostname === "localhost" || loc.protocol === "file:")
+    return warn(notSending + "from localhost");
+
+  // We do advanced bot detection in our API, but this line filters already most bots
+  if (!userAgent || userAgent.search(/(bot|spider|crawl)/gi) > -1)
+    return warn(notSending + "because bot detected");
 
   try {
+    var getParams = function(regex) {
+      // From the search we grab the utm_source and ref and save only that
+      var matches = loc.search.match(
+        new RegExp("[?&](" + regex + ")=([^?&]+)", "gi")
+      );
+      var match = matches
+        ? matches.map(function(m) {
+            return m.split("=")[1];
+          })
+        : [];
+      if (match && match[0]) return match[0];
+    };
+
+    // This code could error on not having resolvedOptions in the Android Webview, that's why we use try...catch
+    var timezone;
+    try {
+      timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    } catch (e) {}
+
+    // We don't want to end up with sensitive data so we clean the referrer URL
+    var utmRegexPrefix = "(utm_)?";
+    var payload = {
+      version: version,
+      hostname: hostname,
+      timezone: timezone,
+      width: window.innerWidth,
+      source: {
+        source: getParams(utmRegexPrefix + "source|ref"),
+        medium: getParams(utmRegexPrefix + "medium"),
+        campaign: getParams(utmRegexPrefix + "campaign"),
+        referrer:
+          (doc.referrer || "")
+            .replace(
+              /^https?:\/\/((m|l|w{2,3}([0-9]+)?)\.)?([^?#]+)(.*)$/,
+              "$4"
+            )
+            .replace(/^([^/]+)\/$/, "$1") || undefined
+      },
+      pageviews: [],
+      events: []
+    };
+
+    // We don't put msHidden in if duration block, because it's used outside of that functionality
+    var msHidden = 0;
+
+    /** if duration **/
+    var hiddenStart = null;
+    window.addEventListener(
+      "visibilitychange",
+      function() {
+        if (doc.hidden) hiddenStart = Date.now();
+        else msHidden += Date.now() - hiddenStart;
+      },
+      false
+    );
+    /** endif **/
+
+    var secondsSince = function(since) {
+      return Math.round((Date.now() - since) / thousand);
+    };
+    var seconds = function() {
+      return Math.round(Date.now() / thousand);
+    };
+
     var sendBeacon = "sendBeacon";
-    var userAgent = nav.userAgent;
     var addEventListenerFunc = window.addEventListener;
     var stringify = JSON.stringify;
     var lastSendUrl;
-    var notSending = "Not sending requests ";
 
     // Safari on iOS < 13 has some issues with the Beacon API
     var useSendBeacon =
-      sendBeacon in window.navigator &&
+      sendBeacon in nav &&
       /ip(hone|ad)(.*)os\s([1-9]|1[0-2])_/i.test(userAgent) === false;
 
     if (useSendBeacon)
@@ -75,7 +143,8 @@
           var last = payload[pageviews][payload[pageviews].length - 1];
 
           /** if duration **/
-          last[duration] = secondsSince(start);
+          last[duration] = secondsSince(start + msHidden);
+          msHidden = 0;
           /** endif **/
 
           /** if scroll **/
@@ -83,19 +152,12 @@
           if (currentScroll) last.scrolled = currentScroll;
           /** endif **/
 
-          if (timezone) payload.timezone = timezone;
-          payload.currentTime = Date.now();
-          navigator[sendBeacon](apiUrl + "post", stringify(payload));
+          payload.time = seconds();
+
+          nav[sendBeacon](apiUrl + "post", stringify(payload));
         },
         false
       );
-
-    var scriptElement = doc.querySelector(
-      'script[src="' + protocol + script + '"]'
-    );
-    var attr = function(scriptElement, attribute) {
-      return scriptElement && scriptElement.getAttribute("data-" + attribute);
-    };
 
     /** if scroll **/
     var scroll = "scroll";
@@ -131,51 +193,29 @@
     });
     /** endif **/
 
-    var mode = attr(scriptElement, "mode");
-    var skipDNT = attr(scriptElement, "skip-dnt") === "true";
-    var functionName = attr(scriptElement, "sa-global") || "sa";
-
-    // We do advanced bot detection in our API, but this line filters already most bots
-    if (!userAgent || userAgent.search(/(bot|spider|crawl)/gi) > -1)
-      return warn(notSending + "because bot detected");
-
-    var utmRegexPrefix = "(utm_)?";
-    var ref = getParams(utmRegexPrefix + "source|ref");
-    var medium = getParams(utmRegexPrefix + "medium");
-    var campaign = getParams(utmRegexPrefix + "campaign");
-
-    // We don't want to end up with sensitive data so we clean the referrer URL
-    var cleanRef =
-      (doc.referrer || "")
-        .replace(/^https?:\/\/((m|l|w{2,3}([0-9]+)?)\.)?([^?#]+)(.*)$/, "$4")
-        .replace(/^([^/]+)\/$/, "$1") || null;
-
-    // This code could error on not having resolvedOptions in the Android Webview, that's why we use try...catch
-    var timezone;
-    try {
-      timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    } catch (e) {}
-
-    var post = function(type, data) {
-      if (useSendBeacon) {
-        var payloadType = payload[type];
-        if (payloadType.length) {
-          /** if duration **/
-          payloadType[payloadType.length - 1].duration = secondsSince(start);
-          /** endif **/
-
-          /** if scroll **/
-          payloadType[payloadType.length - 1].scrolled = scrolled;
-          /** endif **/
-        }
-        payloadType.push(data);
-
+    var post = function(type, data, isPushState) {
+      var payloadType = payload[type];
+      if (payloadType.length) {
         /** if duration **/
-        start = Date.now();
+        payloadType[payloadType.length - 1].duration = secondsSince(
+          start + msHidden
+        );
         /** endif **/
 
         /** if scroll **/
-        scrolled = 0;
+        payloadType[payloadType.length - 1].scrolled = scrolled;
+        /** endif **/
+      }
+      payloadType.push(data);
+
+      if (useSendBeacon) {
+        /** if duration **/
+        start = Date.now();
+        msHidden = 0;
+        /** endif **/
+
+        /** if scroll **/
+        scrolled = window.setTimeout(position, 100);
         /** endif **/
 
         return;
@@ -184,15 +224,21 @@
       var request = new XMLHttpRequest();
       request.open("POST", apiUrl + type, true);
 
+      if (isPushState) {
+        delete payload.source;
+      }
+
+      payload.time = seconds();
+
       // We use content type text/plain here because we don't want to send an
       // pre-flight OPTIONS request
       request.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
-      request.send(stringify(data));
+      request.send(stringify(payload));
     };
 
     var pageview = function(isPushState) {
       // Obfuscate personal data in URL by dropping the search and hash
-      var url = loc.protocol + "//" + hostname + loc.pathname;
+      var url = loc.pathname;
 
       // Add hash to url when script is put in to hash mode
       if (mode === "hash" && loc.hash) url += loc.hash.split("?")[0];
@@ -202,23 +248,10 @@
 
       lastSendUrl = url;
 
-      // Don't track when Do Not Track is set to true
-      if (!skipDNT && doNotTrack in nav && nav[doNotTrack] === "1")
-        return warn(notSending + "when " + doNotTrack + " is enabled");
-
-      // Don't track when localhost
-      if (hostname === "localhost" || loc.protocol === "file:")
-        return warn(notSending + "from localhost");
-
-      var data = { url: url, added: Date.now() };
-      if (ref) data.urlReferrer = ref;
-      if (window.innerWidth) data.width = window.innerWidth;
-
-      if (!isPushState) {
-        if (cleanRef) data.referrer = cleanRef;
-        if (timezone) data.timezone = timezone;
-        data.version = version;
-      }
+      var data = {
+        url: url,
+        added: seconds()
+      };
 
       /** if uniques **/
       // We put new code always in a try block to prevent huge issues
@@ -245,14 +278,14 @@
           isPushState || back
             ? false
             : doc.referrer
-            ? doc.referrer.split("/")[2] !== hostname
+            ? doc.referrer.split(slash)[2] !== hostname
             : true;
       } catch (error) {
         data.error = error.message;
       }
       /** endif **/
 
-      post(pageviews, data);
+      post(pageviews, data, isPushState);
     };
 
     /** if spa **/
@@ -273,6 +306,9 @@
       addEventListenerFunc(pushState, function() {
         pageview(1);
       });
+      window.onpopstate = function() {
+        pageview(1);
+      };
     }
     /** endif **/
 
@@ -288,28 +324,16 @@
     pageview();
 
     /** if events **/
-    var refOrDocRef = ref || cleanRef;
     var queue =
       window[functionName] && window[functionName].q
         ? window[functionName].q
         : [];
 
     window[functionName] = function(event) {
-      post(events, {
-        event: event,
-        ref: refOrDocRef,
-        campaign: campaign,
-        medium: medium
-      });
+      post(events, event);
     };
 
-    for (var index = 0; index < queue.length; index++)
-      post(events, {
-        event: queue[index][0],
-        ref: refOrDocRef,
-        campaign: campaign,
-        medium: medium
-      });
+    for (var event in queue) post(events, queue[event]);
     /** endif **/
   } catch (e) {
     warn(e.message);
