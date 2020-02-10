c78009f336bf1e317462423414925724d0fdacd5
Adriaan van Rossum <git@adriaan.io>
2020-02-10 18:33:48 +0100

    Use Mocha and Chaijs

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
+const { expect } = require("chai");
+
 const browserstack = require("browserstack-local");
-const webdriver = require("selenium-webdriver");
+const { Builder } = require("selenium-webdriver");
 const { promisify } = require("util");
-// const request = require("request");
-// const requestPromise = promisify(request);
 const sleep = promisify(setTimeout);
 const { SERVER_PORT } = require("./constants");
 
@@ -15,78 +15,121 @@ const {
 } = require("./constants/browserstack");
 
 const server = require("./helpers/server");
-// const proxy = require("./helpers/proxy");
 
-const log = (...messages) => console.log("=> Test:", ...messages);
+const log = (...messages) => console.log("    => Test:", ...messages);
 
 if (!BS_USERNAME || !BS_KEY || !BS_BUILD_ID) {
   log("BS_USERNAME, BS_KEY, nor BS_BUILD_ID are not defined.");
   process.exit(1);
 }
 
-let driver;
+const getRequest = (allRequests, params) => {
+  params = { method: "POST", ...params };
+  const keys = Object.keys(params);
+  const request = allRequests.find(request => {
+    const foundKeys = keys.filter(key => {
+      return params[key] === request[key];
+    });
+    return foundKeys.length === keys.length;
+  });
+  return request;
+};
 
-(async () => {
-  log("Starting");
+const navigate = async ({ driver, script }) => {
+  const page = `http://localhost:${SERVER_PORT}/?script=${encodeURIComponent(
+    script
+  )}&push=true`;
 
-  // await proxy();
+  // When iOS and no valid ssl:
+  // await driver.executeScript('browserstack_executor: {"action": "acceptSsl"}');
 
-  const { done, requests } = await server();
+  await driver.get(page);
+  await sleep(2000);
+  await driver.get(`http://localhost:${SERVER_PORT}/empty`);
+  await sleep(500);
+  await driver.close();
+  await sleep(500);
+};
 
-  try {
-    log("start with bs", BS_LOCAL_OPTIONS);
+describe("Collect data", () => {
+  let driver, stopServer, startLocal, stopLocal, requests;
+
+  before(async () => {
     const BrowserStackLocal = new browserstack.Local();
-    const start = promisify(BrowserStackLocal.start).bind(BrowserStackLocal);
-    const stop = promisify(BrowserStackLocal.stop).bind(BrowserStackLocal);
+    startLocal = promisify(BrowserStackLocal.start).bind(BrowserStackLocal);
+    stopLocal = promisify(BrowserStackLocal.stop).bind(BrowserStackLocal);
 
-    await start(BS_LOCAL_OPTIONS);
+    const s = await server();
+    stopServer = s.done;
+    requests = s.requests;
+
+    await startLocal(BS_LOCAL_OPTIONS);
     log("Is running?", BrowserStackLocal.isRunning());
 
-    driver = new webdriver.Builder()
+    driver = await new Builder()
       .usingServer("http://hub-cloud.browserstack.com/wd/hub")
       .withCapabilities(BS_CAPABILITIES)
       .build();
+  });
 
-    const { id_: sessionId } = await driver.session_;
-    log("Session", sessionId);
+  it("should collect page views via pushState", async () => {
+    await navigate({ driver, script: "/latest/hello.js" });
 
-    const script = "/latest/hello.js";
-    const page = `http://localhost:${SERVER_PORT}/?script=${encodeURIComponent(
-      script
-    )}&push=true`;
+    console.log(JSON.stringify(requests, null, 2));
 
-    await driver.get(page);
+    expect(
+      requests,
+      "There should be requests recorded"
+    ).to.have.lengthOf.at.least(2);
 
-    await sleep(2000);
-    await driver.close();
-    await sleep(2000);
+    const postRequest = getRequest(requests, { pathname: "/v2/post" });
+
+    expect(
+      postRequest.body,
+      "All required keys should be present"
+    ).to.include.all.keys(
+      // "timezone",
+      "version",
+      "hostname",
+      "https",
+      "width",
+      "source",
+      "pageviews",
+      "time"
+    );
+
+    expect(
+      postRequest.body.pageviews.length,
+      "There should be 2 page views"
+    ).to.equal(2);
+
+    expect(
+      postRequest.body.pageviews[0],
+      "The first visit should be unique"
+    ).to.have.property("unique", true);
+
+    expect(
+      postRequest.body.pageviews[1],
+      "The second visit should not be unique"
+    ).to.have.property("unique", false);
+
+    expect(
+      postRequest.body.pageviews[1].duration,
+      "Duration should be 2 seconds"
+    ).to.equal(2);
+
+    expect(
+      postRequest.body.time - postRequest.body.pageviews[1].added,
+      "Time between page view and request should be ~2 seconds"
+    ).to.be.within(
+      postRequest.body.pageviews[1].duration - 1,
+      postRequest.body.pageviews[1].duration + 1
+    );
+  });
+
+  after(async () => {
     await driver.quit();
-
-    await done();
-    await stop();
-
-    const foundScript =
-      requests.find(({ pathname } = {}) => pathname === "/script.js") !==
-      undefined;
-
-    const twoPageViews =
-      requests.find(
-        ({ method, pathname, body } = {}) =>
-          method === "POST" &&
-          pathname === "/v2/post" &&
-          body &&
-          body.pageviews &&
-          body.pageviews.length === 2
-      ) !== undefined;
-
-    // log("Requests", requests);
-
-    log("Found script", foundScript);
-    log("Has two page views (beacon)", twoPageViews);
-  } catch ({ message }) {
-    log("Error:", message);
-    if (driver) await driver.quit();
-
-    process.exit(1);
-  }
-})();
+    await stopLocal();
+    await stopServer();
+  });
+});
