d1ed075fb1423d6b1e284c4af3a42a8a2a02051c
Adriaan van Rossum <git@adriaan.io>
2020-02-28 15:18:41 +0100

    Use first browser in localBrowserFilter

--- test/index.js
+++ test/index.js
@@ -18,7 +18,7 @@ const {
 } = require("./constants/browserstack");
 
 const localBrowserFilter = ({ browser, browser_version, os, os_version }) =>
-  browser === "chrome" && version(browser_version) == 49; // os === "ios" && os == "Windows" && os_version == "8";
+  true; // browser === "chrome" && version(browser_version) == 49; // os === "ios" && os == "Windows" && os_version == "8";
 
 const getSeleniumVersion = ({ browser, os, browser_version }) => {
   const isMobile = ["ios", "android"].includes(os);
