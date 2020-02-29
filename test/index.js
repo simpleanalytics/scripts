52e6299b8153a9c64527d1d5647f08a3e27bcff0
Adriaan van Rossum <git@adriaan.io>
2020-02-29 11:40:37 +0100

    Test with OS X Lion with chrome 49.0

--- test/index.js
+++ test/index.js
@@ -18,10 +18,10 @@ const {
 } = require("./constants/browserstack");
 
 const localBrowserFilter = ({ browser, browser_version, os, os_version }) =>
-  browser === "ie" &&
-  version(browser_version) == 11 &&
-  os == "Windows" &&
-  os_version == "7"; // os === "ios" && ;
+  browser === "chrome" &&
+  version(browser_version) == 49 &&
+  os == "OS X" &&
+  os_version == "Lion"; // os === "ios" && ;
 
 const getSeleniumVersion = ({ browser, os, browser_version }) => {
   const isMobile = ["ios", "android"].includes(os);
