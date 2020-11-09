5ed4851c22c15913af8a78e27f0cff7080be8fa4
adriaanvanrossum <adriaanvanrossum@users.noreply.github.com>
2020-11-09 21:29:09 +0000

    Run prettier via GitHub Action

--- src/default.js
+++ src/default.js
@@ -453,7 +453,7 @@
 
     var getPath = function (overwrite) {
       var path = "";
-      
+
       // decodeURIComponent can fail when having invalid characters
       // https://github.com/simpleanalytics/roadmap/issues/462
       try {
