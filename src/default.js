c0c442476cfc3d4d303d3e388509a7111fb12916
Dave Jansen <i.am@davejansen.com>
2021-02-05 18:10:55 +0900

    Remove optional leading semicolon as we don't have to be that specific, and the semicolon may appear elsewhere too.

--- src/default.js
+++ src/default.js
@@ -77,7 +77,7 @@
       try {
         payload.mobile = uaData.mobile;
         
-        var ex = new RegExp(/\;?(not|totally)(.*)(brand)/i);
+        var ex = new RegExp(/(not|totally)(.*)(brand)/i);
         var parsedBrands = [];
         for (var i=0,m=uaData.brands.length; i<m; i++) {
           var b = uaData.brands[i];
