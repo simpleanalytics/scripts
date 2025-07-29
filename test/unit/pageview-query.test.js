9294410a19e015898ae637e4f20bf4d55280b979
Adriaan <1079135+adriaandotcom@users.noreply.github.com>
2025-07-29 15:36:14 +0200

    fix allowParams regex to be case insensitive

--- test/unit/pageview-query.test.js
+++ test/unit/pageview-query.test.js
@@ -19,4 +19,22 @@ describe("pageview query", function () {
       done();
     }, 10);
   });
+
+  it("matches allowed params case insensitively", function (done) {
+    const dom = createDOM({
+      settings: { autoCollect: false, allowParams: "foo" },
+    });
+
+    dom.window.sa_pageview("/manual?FOO=bar");
+
+    setTimeout(() => {
+      const req = dom.sent.find(
+        (r) => r.type === "image" && /path=%2Fmanual/.test(r.url)
+      );
+      expect(req, "pageview request").to.exist;
+      const url = new URL(req.url);
+      expect(url.searchParams.get("query")).to.equal("FOO=bar");
+      done();
+    }, 10);
+  });
 });
